4b97e8479cd91557aca2239143d2cc8c71ff157d
Luiz Gustavo Ianoski <158396659+ianoski7@users.noreply.github.com>
2024-03-10 13:08:59 -0300

    Add files via upload

---
+++ avaliativa01/php/hip.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arquivo php</title>
+    <style>
+        body {
+            background: linear-gradient(to right, rgb(0, 6, 69) 0%, rgb(65, 68, 255) 51%, rgb(0, 6, 69) 100%);
+            color: white;
+            font-size: 40px;
+            font-family: cursive;
+            text-align: center;
+            margin-top: 100px;
+        }
+        p {
+            margin-top: 250px;
+        }   
+        a.voltar {
+            color: #fff;
+            border: 4px solid black;
+            background: linear-gradient(95deg, rgb(0, 255, 242) 0%, rgb(0, 25, 254) 50%, rgba(0,255,242,1) 100%);
+            border-radius: 20px;
+            margin-top: 50px;
+            display: inline-block;
+            padding: 10px 20px;
+            text-decoration: none;
+            transition: transform 0.3s ease;
+        }
+        a.voltar:hover {
+            color: black;
+            background: linear-gradient(95deg, rgba(0,25,254,1) 0%, rgba(0,255,242,1) 50%, rgba(0,25,254,1) 100%);
+            height: 50px;
+            width: 135px;
+            box-shadow: rgba(0, 0, 0, 0.5) 0px 54px 55px, rgba(0, 0, 0, 0.5) 0px -12px 30px, rgba(0, 0, 0, 0.5) 0px 4px 6px, rgba(0, 0, 0, 0.5) 0px 12px 13px, rgba(0, 0, 0, 0.5) 0px -3px 5px;
+            transform: scale(1.1);
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $cat1 = $_GET["cat1"];
+        $cat2 = $_GET["cat2"];
+        echo "<strong><p> o valor da hipotenusa é: " . hypot($cat1, $cat2) . "</p>";
+    ?>
+    <a href="http://localhost/Back-End/avaliativa01/" class="voltar">Voltar</a>
+</body>
+</html>
---
+++ avaliativa01/php/max.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background: linear-gradient(to right, rgb(0, 6, 69) 0%, rgb(65, 68, 255) 51%, rgb(0, 6, 69) 100%);
+            color: white;
+            font-size: 40px;
+            font-family: cursive;
+            text-align: center;
+            margin-top: 100px;
+        }
+        p {
+            margin-top: 90px;
+            margin-bottom: 80px;
+        }   
+        a.voltar {
+            color: #fff;
+            border: 4px solid black;
+            background: linear-gradient(95deg, rgb(0, 255, 242) 0%, rgb(0, 25, 254) 50%, rgba(0,255,242,1) 100%);
+            border-radius: 20px;
+            margin-top: 50px;
+            margin-bottom: 50px; 
+            display: inline-block;
+            padding: 10px 20px;
+            text-decoration: none;
+            transition: transform 0.3s ease;
+        }
+        a.voltar:hover {
+            color: black;
+            background: linear-gradient(95deg, rgba(0,25,254,1) 0%, rgba(0,255,242,1) 50%, rgba(0,25,254,1) 100%);
+            height: 50px;
+            width: 135px;
+            box-shadow: rgba(0, 0, 0, 0.5) 0px 54px 55px, rgba(0, 0, 0, 0.5) 0px -12px 30px, rgba(0, 0, 0, 0.5) 0px 4px 6px, rgba(0, 0, 0, 0.5) 0px 12px 13px, rgba(0, 0, 0, 0.5) 0px -3px 5px;
+            transform: scale(1.1);
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $num1 = $_GET["numerooo1"];
+        $num2 = $_GET["numerooo2"];
+        $num3 = $_GET["numerooo3"];
+
+        echo "<strong><p> O maior número entre os três números informados é: " . max($num1, $num2, $num3) . "</p>";
+    ?>
+    <a href="http://localhost/Back-End/avaliativa01/" class="voltar">Voltar</a>
+
+</body>
+</html>
